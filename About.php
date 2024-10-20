831c82b547b840b451852ef7a1d9df434036a3b4
Madhav-26 <jagtapmadhav395@gmail.com>
2024-10-20 21:41:03 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,48 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="div1.png" alt="showcase image" style="height:280px; width:540px;" >
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>Our Mission</h3>
+						<p>Welcome to Talent Track, your trusted platform for connecting job seekers and employers. We are committed to making the job search and hiring process easier, faster, and more efficient for everyone. Whether you're looking to find your dream job or the perfect candidate, we've got you covered.
+
+At Talent Track, we understand the challenges of navigating today’s competitive job market. That’s why we provide a user-friendly platform where job seekers can explore a wide range of career opportunities, and employers can find the best talent to join their teams.</p>
+						 <p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo..</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					
+							
+							
+							
+						</div>
+						
+						 						
+						 
+						<br>
+					
+				
+						
+						
+						
+					
+						
+						
+						
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
